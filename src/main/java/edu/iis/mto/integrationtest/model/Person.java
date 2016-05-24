59c39d27c5e8d6db9de56576cc414fa15693b528
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-05-24 23:14:03 +0200

    Added JPA annotations to Person class

--- src/main/java/edu/iis/mto/integrationtest/model/Person.java
+++ src/main/java/edu/iis/mto/integrationtest/model/Person.java
@@ -1,38 +1,48 @@
 package edu.iis.mto.integrationtest.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @Column(unique = true, nullable = false)
+    private Long id;
 
-	private Long id;
+    @Column(name = "first_name")
+    private String firstName;
 
-	private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
-	private String lastName;
+    public Person() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Person() {
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
